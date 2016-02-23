d5cd4aba8ad32f042f22339b44296a20bdd89a55
Marybeth Snodgrass <msnodgrass25@gmail.com>
2016-02-23 09:24:40 -0600

    commit from 2/23 AM

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const userRoutes = require('./lib/user/routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+app.use(userRoutes);
+
+app.locals.title = '';
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || { email: 'Guest' };
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+mongoose.connect('mongodb://localhost:27017/nodeauth', (err) => {
+  if (err) throw err;
+
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
